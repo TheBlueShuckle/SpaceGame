0197faf4cdcff5a4b1df2bc8ff0d28f825ccdca7
Ville Halldin <villehalldin05@outlook.com>
2022-11-29 23:09:09 +0100

    Add project files.

---
+++ SpaceGame/Game1.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D myShip;
+        Vector2 myShipPos;
+        Vector2 myShipSpeed;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            myShipPos.X = 100;
+            myShipPos.Y = 100;
+            myShipSpeed.X = 2.5f;
+            myShipSpeed.Y = 2.5f;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            myShip = Content.Load<Texture2D>("Sprites/myShip");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            CheckMove();
+            CheckBoundsX();
+            CheckBoundsY();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(myShip, myShipPos, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public void CheckMove()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                myShipPos.X = myShipPos.X - myShipSpeed.X;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                myShipPos.X = myShipPos.X + myShipSpeed.X;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                myShipPos.Y = myShipPos.Y - myShipSpeed.Y;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                myShipPos.Y = myShipPos.Y + myShipSpeed.Y;
+            }
+        }
+
+        public void CheckBoundsX()
+        {
+            if ((myShipPos.X >= (Window.ClientBounds.Width - myShip.Width) && Keyboard.GetState().IsKeyDown(Keys.D)) || (myShipPos.X <= 0 && Keyboard.GetState().IsKeyDown(Keys.A)))
+            {
+                if (myShipPos.X < 0)
+                {
+                    myShipPos.X = 0;
+                }
+
+                if (myShipPos.X >= Window.ClientBounds.Width - myShip.Width)
+                {
+                    myShipPos.X = Window.ClientBounds.Width - myShip.Width;
+                }
+
+                myShipSpeed.X = 0;
+            }
+
+            else
+            {
+                myShipSpeed.X = 2.5f;
+            }
+        }
+
+        public void CheckBoundsY()
+        {
+            if ((myShipPos.Y >= (Window.ClientBounds.Height - myShip.Height) && Keyboard.GetState().IsKeyDown(Keys.S)) || (myShipPos.Y <= 0 && Keyboard.GetState().IsKeyDown(Keys.W)))
+            {
+                if (myShipPos.Y < 0)
+                {
+                    myShipPos.Y = 0;
+                }
+
+                if (myShipPos.Y >= Window.ClientBounds.Height - myShip.Height)
+                {
+                    myShipPos.Y = Window.ClientBounds.Height - myShip.Height;
+                }
+
+                myShipSpeed.Y = 0;
+            }
+            else
+            {
+                myShipSpeed.Y = 2.5f;
+            }
+        }
+    }
+}
---
+++ SpaceGame/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new SpaceGame.Game1();
+game.Run();
