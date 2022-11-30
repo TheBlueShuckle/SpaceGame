82b028845c17a28d931ad372f1b3bc97da3d43d7
Ville Halldin <villehalldin05@outlook.com>
2022-11-30 18:09:17 +0100

    La till get-metoder i planet-klassen

--- SpaceGame/Planet.cs
+++ SpaceGame/Planet.cs
@@ -17,17 +17,17 @@ namespace SpaceGame
         Color planetColor;
         Vector2 location;
 
-        public Planet(Texture2D bigPlanet, Texture2D smallPlanet)
+        public Planet(Texture2D bigPlanet, Texture2D smallPlanet, int windowHeight, int windowWidth)
         {
             this.bigPlanet = bigPlanet;
             this.smallPlanet = smallPlanet;
 
-            GenerateColor();
-            GenerateLocation(600, 800);
-            GenerateSize();
+            GeneratePlanetColor();
+            GeneratePlanetLocation(windowHeight, windowWidth);
+            GeneratePlanetSize();
         }
 
-        public void GenerateSize()
+        private void GeneratePlanetSize()
         {
             Texture2D[] planetSizes = new Texture2D[2] { smallPlanet, bigPlanet };
             Random rnd = new Random();
@@ -36,7 +36,7 @@ namespace SpaceGame
             size = planetSizes[randomSize];
         }
 
-        public void GenerateColor()
+        private void GeneratePlanetColor()
         {
             Color[] colors = new Color[9] { Color.SandyBrown, Color.Aquamarine, Color.CornflowerBlue, Color.Crimson, Color.Beige, Color.Green, Color.Coral, Color.GreenYellow, Color.RoyalBlue };
             Random rnd = new Random();
@@ -45,13 +45,28 @@ namespace SpaceGame
             planetColor = colors[randomColor];
         }
 
-        public void GenerateLocation(int height, int width)
+        private void GeneratePlanetLocation(int height, int width)
         {
             Random rnd = new Random();
             int randomHeight = rnd.Next(0, height), randomWidth = rnd.Next(0, width);
 
             location.Y = randomHeight;
             location.X = randomWidth;
+        }
+
+        public Texture2D GetPlanetSize()
+        {
+            return size;
+        }
+
+        public Color GetPlanetColor()
+        {
+            return planetColor;
+        }
+
+        public Vector2 GetPlanetLocation()
+        {
+            return location;
         }
     }
 }
