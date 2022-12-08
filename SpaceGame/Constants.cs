94cc6bc9448fc48734e34e177e873a60566e6e4f
Ville Halldin <villehalldin05@outlook.com>
2022-12-08 14:30:35 +0100

    Var inte riktigt klar

--- SpaceGame/Constants.cs
+++ SpaceGame/Constants.cs
@@ -9,7 +9,7 @@ namespace SpaceGame
     public static class Constants
     {
         public const int InSpace = 1, OnPlanet = 2;
-        public const int FirstPlanetWait = 3, PlanetMinWait = 5, PlanetMaxWait = 20;
+        public const int PlanetMinWait = 5, PlanetMaxWait = 20;
         public const int ProtagonistSpeedConst = 5;
         public const float MyShipSpeedConst = 2.5f;
     }
